14cece4819cbf0364f5d91a4116850631b6a39da
Dennis <denniszelada@gmail.com>
2015-07-11 20:45:17 -0300

    added tastypie settings

--- admetricks/admetricks/settings.py
+++ admetricks/admetricks/settings.py
@@ -101,6 +101,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+#Tastypie
+#http://tastypieapi.org
+TASTYPIE_ALLOW_MISSING_SLASH = true
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
