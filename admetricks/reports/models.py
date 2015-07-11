5b4ba8c3c9a9867792f41a0391169edbd5dd9923
Dennis <denniszelada@gmail.com>
2015-07-11 20:42:58 -0300

    added the model campaign to the reports app

--- admetricks/reports/models.py
+++ admetricks/reports/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Campaign(models.Model):
+    """
+    Campaign
+    --------------------
+    This class allows access to data stored under the 'test' table.
+
+    """
+
+    date = models.DateField(db_column='fecha')
+    advisor = models.CharField(max_length=255, db_column='avisador')
+    campaign = models.Charfield(max_length=255, db_column='campana')
+    media = models.CharField(max_length=255, db_column='medio')
+    impact = models.IntegerField(db_column='impacto')
+    banner = models.URLField(max_length=255)
+    class Meta:
+        db_table = 'test'
