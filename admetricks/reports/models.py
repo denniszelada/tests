10a5caf8bf8f4e8c5e23a8db110c5bc043ee58e4
Dennis <denniszelada@gmail.com>
2015-07-12 14:04:08 -0300

    added a class for serialization on the reports app

--- admetricks/reports/models.py
+++ admetricks/reports/models.py
@@ -1,5 +1,21 @@
 from django.db import models
 
+class CampaignSumarry(object):
+    def __init__(self, initial=None):
+        self.__dict__['_data'] = {}
+
+        if hasattr(initial, 'items'):
+            self.__dict__['_data'] = initial
+
+    def __getattr__(self, name):
+        return self._data.get(name, None)
+
+    def __setattr__(self, name):
+        self.__dict__['_data'][name] = value
+
+    def to_dict(self):
+        return self._data
+
 class Campaign(models.Model):
     """
     Campaign
@@ -9,10 +25,10 @@ class Campaign(models.Model):
     """
 
     date = models.DateField(db_column='fecha')
-    advisor = models.CharField(max_length=255, db_column='avisador')
-    campaign = models.CharField(max_length=255, db_column='campanya')
-    media = models.CharField(max_length=255, db_column='medio')
-    impact = models.IntegerField(db_column='impacto')
-    banner = models.URLField(max_length=255)
+    advisor = models.CharField(max_length=255, db_column='avisador', null=True)
+    campaign = models.CharField(max_length=255, db_column='campanya', null=True)
+    media = models.CharField(max_length=255, db_column='medio', null=True)
+    impact = models.IntegerField(db_column='impacto', null=True)
+    banner = models.URLField(max_length=255, null=True)
     class Meta:
         db_table = 'test'
