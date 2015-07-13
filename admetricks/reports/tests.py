f4e832ef7e7ba3912940486015b1c7252788b4c5
Dennis <denniszelada@gmail.com>
2015-07-13 16:08:16 -0300

    updated tests

--- admetricks/reports/tests.py
+++ admetricks/reports/tests.py
@@ -45,10 +45,152 @@ class CampaignResourceTest(ResourceTestCase):
         self.assertEqual(len(object_list),2)
         self.assertKeys(object_list['objects'][0],['impact', 'campaign','media','advisor','date','banners'])
 
-    def test_get_detail_json(self):
-        resp = self.api_client.get(self.detail_url, format='json')
+class AdvisorsResourceTest(ResourceTestCase):
+    fixtures = ['test-campaigns.json']
+
+    def setUp(self):
+        super(AdvisorsResourceTest, self).setUp()
+
+        self.campaign = Campaign.objects.get(id=1)
+
+        self.detail_url = '/api/v1/advisors/{0}/'.format(self.campaign.advisor)
+
+    """
+    Verifica el acceso al recurso de campanya sin un header de autorizacion
+    """
+    def test_get_list_unauthorized(self):
+        resp = self.api_client.get('/api/v1/advisors/')
+        self.assertHttpOK(resp)
+
+    """
+    Verifica que la respuesta del servidor sea un 405 en los metodos no permitidos,
+    post, put, delete
+    """
+    def test_not_allowed_methods(self):
+        data = {'data':'empty data or anything'}
+        resp = self.api_client.post('/api/v1/campaigns/',data=data)
+        self.assertHttpMethodNotAllowed(resp)
+
+        resp = self.api_client.put(self.detail_url ,data=data)
+        self.assertHttpMethodNotAllowed(resp)
+
+        resp = self.api_client.delete(self.detail_url)
+        self.assertHttpMethodNotAllowed(resp)
+
+    """
+    Verifica que la respuesta del servidor sea un 405 en los metodos no permitidos,
+    post, put, delete
+    """
+    def test_not_allowed_methods(self):
+        data = {'data':'empty data or anything'}
+        resp = self.api_client.post('/api/v1/advisors/',data=data)
+        self.assertHttpMethodNotAllowed(resp)
+
+        resp = self.api_client.put(self.detail_url ,data=data)
+        self.assertHttpMethodNotAllowed(resp)
+
+        resp = self.api_client.delete(self.detail_url)
+        self.assertHttpMethodNotAllowed(resp)
+
+    """
+    Obtiene la primera pagina de recursos
+    """
+    def test_get_list_json(self):
+        resp = self.api_client.get('/api/v1/advisors/', format='json')
         self.assertValidJSONResponse(resp)
 
-        response_object = self.deserialize(resp)
-        self.assertKeys(response_object, ['impact', 'campaign','media','advisor','date','banner','id','resource_uri'])
-        self.assertEqual(response_object['campaign'], 'Autoexperto')
+        object_list = self.deserialize(resp)
+        self.assertEqual(len(object_list['objects']), 12)
+        self.assertEqual(len(object_list),2)
+        self.assertKeys(object_list['objects'][0],['advisor', 'campaigns'])
+
+class MediaResourceTest(ResourceTestCase):
+    fixtures = ['test-campaigns.json']
+
+    def setUp(self):
+        super(MediaResourceTest, self).setUp()
+
+        self.campaign = Campaign.objects.get(id=1)
+
+        self.detail_url = '/api/v1/media/{0}/'.format(self.campaign.advisor)
+
+    """
+    Verifica el acceso al recurso de campanya sin un header de autorizacion
+    """
+    def test_get_list_unauthorized(self):
+        resp = self.api_client.get('/api/v1/media/')
+        self.assertHttpOK(resp)
+
+    """
+    Verifica que la respuesta del servidor sea un 405 en los metodos no permitidos,
+    post, put, delete
+    """
+    def test_not_allowed_methods(self):
+        data = {'data':'empty data or anything'}
+        resp = self.api_client.post('/api/v1/media/',data=data)
+        self.assertHttpMethodNotAllowed(resp)
+
+        resp = self.api_client.put(self.detail_url ,data=data)
+        self.assertHttpMethodNotAllowed(resp)
+
+        resp = self.api_client.delete(self.detail_url)
+        self.assertHttpMethodNotAllowed(resp)
+
+    """
+    Obtiene la primera pagina de recursos
+    """
+    def test_get_list_json(self):
+        resp = self.api_client.get('/api/v1/media/', format='json')
+        self.assertValidJSONResponse(resp)
+
+        object_list = self.deserialize(resp)
+        self.assertEqual(len(object_list['objects']), 20)
+        self.assertEqual(len(object_list),2)
+        self.assertKeys(object_list['objects'][0],['media', 'campaigns'])
+
+class ChartResourceTest(ResourceTestCase):
+    fixtures = ['test-campaigns.json']
+
+    def setUp(self):
+        super(ChartResourceTest, self).setUp()
+
+        self.campaign = Campaign.objects.get(id=1)
+
+        self.detail_url = '/api/v1/chart/{0}/'.format(self.campaign.advisor)
+
+    """
+    Verifica el acceso al recurso de campanya sin un header de autorizacion
+    """
+    def test_get_list_unauthorized(self):
+        resp = self.api_client.get('/api/v1/chart/')
+        self.assertHttpOK(resp)
+
+    """
+    Verifica que la respuesta del servidor sea un 405 en los metodos no permitidos,
+    post, put, delete
+    """
+    def test_not_allowed_methods(self):
+        data = {'data':'empty data or anything'}
+        resp = self.api_client.post('/api/v1/chart/',data=data)
+        self.assertHttpMethodNotAllowed(resp)
+
+        resp = self.api_client.put(self.detail_url ,data=data)
+        self.assertHttpMethodNotAllowed(resp)
+
+        resp = self.api_client.delete(self.detail_url)
+        self.assertHttamostpMethodNotAllowed(resp)
+
+    """
+    Obtiene la primera pagina de recursos
+    """
+    def test_get_list_json(self):
+        resp = self.api_client.get('/api/v1/chart/', format='json')
+        self.assertValidJSONResponse(resp)
+
+        object_list = self.deserialize(resp)
+        self.assertEqual(len(object_list['objects']), 2)
+        self.assertEqual(len(object_list),2)
+        self.assertKeys(object_list['objects'],['datasets', 'labels'])
+        self.assertKeys(object_list['objects']['datasets'][0],
+                    ['data','fillColor','label','pointColor',
+                       'pointHighlightStroke', 'pointStrokeColor','strokeColor'])
